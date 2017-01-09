8d13e995168efaaacd5efcbe40c5cd1a7fcf9229
Edward Benson <edward.benson@gmail.com>
2017-01-09 11:02:18 -0800

    Basic compilation works

--- gulpfile.js
+++ gulpfile.js
@@ -7,47 +7,34 @@ var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var merge2 = require('merge2');
 
-// gulp.task('build', function () {
-//   var clientProject = ts.createProject({
-//     declaration: true,
-//     noResolve: true,
-//     module: 'system',
-//     // typescript: require('typescript'),
-//     target: 'es5',
-//     outFile: 'inside.js',
-//     sourceMap: true,
-//     formatCodeOptions: {
-//       "indentSize": 2,
-//       "tabSize": 2,
-//       "convertTabsToSpaces": true,
-//       "insertSpaceAfterCommaDelimiter": true,
-//       "insertSpaceAfterSemicolonInForStatements": true,
-//       "insertSpaceBeforeAndAfterBinaryOperators": true,
-//       "insertSpaceAfterKeywordsInControlFlowStatements": true,
-//       "insertSpaceAfterFunctionKeywordForAnonymousFunctions": false,
-//       "insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis": false,
-//       "placeOpenBraceOnNewLineForFunctions": false,
-//       "placeOpenBraceOnNewLineForControlBlocks": false
-//     }
-//   });
+gulp.task('build', function () {
+  var clientProject = ts.createProject({
+    declaration: true,
+    noResolve: true,
+    module: 'system',
+    // typescript: require('typescript'),
+    target: 'es5',
+    outFile: 'cloudstitch-cli-no-dependencies.js',
+    sourceMap: true
+  });
   
-//   var files = [
-//     'lib/**/*.ts'
-//   ];
+  var files = [
+    'src/**/*.ts'
+  ];
 
-//   var tsResult = gulp.src(files)
-//                     // .pipe(sourcemaps.init())
-//                     .pipe(clientProject());
+  var tsResult = gulp.src(files)
+                    // .pipe(sourcemaps.init())
+                    .pipe(clientProject());
 
-//   tsResult.dts.pipe(gulp.dest('./bin'));
-  
-//   var dependencies = gulp.src([
+  tsResult.dts.pipe(gulp.dest('./bin'));
+    
+  var dependencies = gulp.src([
+  ]);
 
-//   ]);
-//   merge2(
-//     dependencies,      
-//     tsResult.js
-//   ).pipe(
-//     concat('inside.js')
-//   ).pipe(gulp.dest(config.output.clientTypescript));
-// });
+  merge2(
+    dependencies,      
+    tsResult.js
+  ).pipe(
+    concat('cloudstitch-cli.js')
+  ).pipe(gulp.dest('./bin'));
+});
