edbc4c7293c1607a82a7fe3d8da34bfea3a5de41
Edward Benson <edward.benson@gmail.com>
2017-04-27 15:55:07 -0700

    Use Web token

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -43,7 +43,7 @@ class Login implements ICommand {
       Request.post("/session", {
       user: answeres["username"],
       password: answeres["password"],
-      desiredResponse: "token"
+      desiredResponse: "web-token"
     }).then((result) => {
       if(result.body.token) {
         config.set("ApiKey", result.body.token);
